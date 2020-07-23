ec523f34e95f04921b885e91880d3912a4ad6e3f
Christina Meador <christinameador1@gmail.com>
2020-07-23 12:14:59 -0500

    added Java graded assignments 1-4

---
+++ Java Web Development/Graded Assignments/2. Tech Jobs (Object-Oriented Edition)/src/org/launchcode/techjobs_oo/Job.java
@@ -0,0 +1,99 @@
+package org.launchcode.techjobs_oo;
+
+import java.util.Objects;
+
+public class Job {
+
+    private int id;
+    private static int nextId = 1;
+
+    private String name;
+    private Employer employer;
+    private Location location;
+    private PositionType positionType;
+    private CoreCompetency coreCompetency;
+
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
+
+    @Override
+    public String toString(){
+            return "ID: " + this.id + " \n" +
+                    "Name: " + this.name + "\n" +
+                    "Employer: " + this.employer + " \n" +
+                    "Location: " + this.location + " \n" +
+                    "Position Type: " + this.positionType + " \n" +
+                    "Core Competency: " + this.coreCompetency + " ";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Job)) return false;
+        Job job = (Job) o;
+        return getId() == job.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    public String getName() {
+        if(this.name.equals("")){
+            return "Data not available";
+        }
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(PositionType positionType) {
+        this.positionType = positionType;
+    }
+
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setCoreCompetency(CoreCompetency coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ Java Web Development/Graded Assignments/3. Tech Jobs (MVC Edition)/src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -0,0 +1,46 @@
+package org.launchcode.javawebdevtechjobsmvc.controllers;
+
+import org.launchcode.javawebdevtechjobsmvc.models.Job;
+import org.launchcode.javawebdevtechjobsmvc.models.JobData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping("search")
+public class SearchController {
+
+    @RequestMapping(value = "")
+    public String search(Model model) {
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
+
+    @PostMapping(value="results")
+    public String displaySearchResults (Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+
+        ArrayList<Job> jobs;
+
+        if(searchTerm.toLowerCase().equals("all") || searchTerm.isEmpty()){
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+
+
+        return "search";
+    }
+
+
+}
